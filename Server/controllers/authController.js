327a2cf8c977cb55587a81068bac746fb3bcbb64
shubham79a <shubh79.nolan@gmail.com>
2025-01-11 19:36:54 +0530

    prod

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -105,7 +105,7 @@ export const logout = async (req, res) => {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
             sameSite: process.env.NODE_ENV === "production" ? 'none' : 'strict',
-            // maxAge: 7 * 24 * 60 * 60 * 1000,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
         })
 
         return res.json({ success: true, message: "Logged Out" })
